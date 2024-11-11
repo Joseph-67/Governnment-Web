43da9d7f2679c5563f912b26127e9a658f5dc28b
abolarin babatunde <abolarinbabatunde88@gmail.com>
2024-11-11 06:58:02 -0800

    summary

--- GovernmentPr/routes/web.php
+++ GovernmentPr/routes/web.php
@@ -14,10 +14,21 @@ use App\Http\Controllers\HomePageController;
 |
 */
 
+<<<<<<< HEAD
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 Route::get('/home', [HomePageController::class, 'displayHome'])->name('home');
+=======
+    // Route::get('/', function () {
+    //     return Inertia::render('Welcome', [
+    //         'canLogin' => Route::has('login'),
+    //         'canRegister' => Route::has('register'),
+    //         'laravelVersion' => Application::VERSION,
+    //         'phpVersion' => PHP_VERSION,
+    //     ]);
+    // });
+>>>>>>> parent of 7b9c021 (Merge branch 'main' of https://github.com/Joseph-67/Governnment-Web)
 
 Route::middleware([
     'auth:sanctum',
