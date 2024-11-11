380dd19db2b41cda558874b4680d613a3e723baf
Anthony Oloyede <oloyede.tony@gmail.com>
2024-11-11 09:29:24 -0500

    summay

--- GovernmentPr/routes/web.php
+++ GovernmentPr/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomePageController;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,17 +15,21 @@ use App\Http\Controllers\HomePageController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/home', [HomePageController::class, 'displayHome'])->name('home');
+    // Route::get('/', function () {
+    //     return Inertia::render('Welcome', [
+    //         'canLogin' => Route::has('login'),
+    //         'canRegister' => Route::has('register'),
+    //         'laravelVersion' => Application::VERSION,
+    //         'phpVersion' => PHP_VERSION,
+    //     ]);
+    // });
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified'
+    'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return Inertia::render('Dashboard');
     })->name('dashboard');
 });
