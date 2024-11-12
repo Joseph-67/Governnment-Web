64bfcbef872666addf539ff39046a28ec0842952
abolarin babatunde <abolarinbabatunde88@gmail.com>
2024-11-12 02:00:33 -0800

    Summary

--- GovernmentPr/app/Actions/Fortify/CreateNewUser.php
+++ GovernmentPr/app/Actions/Fortify/CreateNewUser.php
@@ -20,10 +20,13 @@ class CreateNewUser implements CreatesNewUsers
     public function create(array $input): User
     {
         Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'first_name' => ['required', 'string', 'min:4', 'max:255'],
+            'last_name' => ['required', 'string', 'min:4', 'max:255'],
+            'other_name' => ['nullable', 'string', 'min:4', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'min:4', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+            'mobile_number' => ['nullable', 'numeric', 'min:12']
         ])->validate();
 
         return User::create([
