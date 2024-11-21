aa1a41a74792cd3e4b96490d4f3b4f1672b50820
abolarin babatunde <abolarinbabatunde88@gmail.com>
2024-11-21 05:16:43 -0800

    summary

--- GovernmentPr/resources/views/components/form-section.blade.php
+++ GovernmentPr/resources/views/components/form-section.blade.php
@@ -1,10 +1,10 @@
-<div {{ $attributes->merge(['class' => 'd-flex ']) }}>
+<div {{ $attributes->merge(['class' => 'row ']) }}>
     <x-section-title>
         <x-slot name="title">{{ $title }}</x-slot>
         <x-slot name="description">{{ $description }}</x-slot>
     </x-section-title>
 
-    <div class="mt-lg-3 col-md-12">
+    <div class="mt-lg-3 col-md-6">
         <form wire:submit.prevent="{{ $submit }}">
             <div class="px-2 py-3 card card-body p-5 shadow-sm rounded">
                 <div class=" g-2">
@@ -13,7 +13,7 @@
             </div>
 
             @if (isset($actions))
-                <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
+                <div class="d-flex items-center justify-end px-4 py-3 text-right px-5 shadow-sm rounded-sm">
                     {{ $actions }}
                 </div>
             @endif
--- GovernmentPr/resources/views/components/section-title.blade.php
+++ GovernmentPr/resources/views/components/section-title.blade.php
@@ -1,8 +1,8 @@
-<div class="col-sm-12 d-flex justify-between">
-    <div class="px-4 px-sm-0">
-        <h3 class="text-lg font-medium text-dark-50">{{ $title }}</h3>
+<div class="col-md-4">
+    <div class="px-5 px-sm-0">
+        <h3 class="text-lg font-medium text-dark">{{ $title }}</h3>
 
-        <p class="mt-1 text-sm text-dark-50">
+        <p class="mt-1 text-sm text-dark">
             {{ $description }}
         </p>
     </div>
