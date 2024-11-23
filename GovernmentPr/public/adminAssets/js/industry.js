8ad6d277e5663d89de8f2fa3e3731b88d18fc1a4
abolarin babatunde <abolarinbabatunde88@gmail.com>
2024-11-23 03:25:49 -0800

    Create industry.js

---
+++ GovernmentPr/public/adminAssets/js/industry.js
@@ -0,0 +1,86 @@
+{
+    "Agriculture" : ["Crop Production (e.g., cassava, maize, rice)"]
+
+
+Livestock Farming
+Fisheries and Aquaculture
+Forestry
+Mining and Quarrying
+
+Oil and Gas
+Coal Mining
+Limestone and Cement Production
+Gold and Precious Minerals
+Energy and Power Generation
+
+Hydroelectric Power
+Renewable Energy (e.g., solar, wind)
+Secondary Sector (Manufacturing & Processing)
+Manufacturing
+
+Food and Beverage Processing
+Textile and Garment Production
+Plastics and Packaging
+Chemicals and Fertilizers
+Building Materials
+Construction
+
+Real Estate Development
+Infrastructure Projects (e.g., roads, bridges)
+Petrochemicals
+
+Refining Crude Oil
+Lubricants and Petrochemical Products
+Tertiary Sector (Services & Technology)
+Banking and Finance
+
+Commercial and Microfinance Banks
+Fintech and Mobile Payments
+Insurance
+Telecommunications
+
+Mobile Network Providers
+Internet Service Providers
+Information Technology
+
+Software Development
+IT Infrastructure and Cloud Services
+Transport and Logistics
+
+Freight and Haulage
+Shipping and Maritime Services
+Aviation
+Retail and Wholesale Trade
+
+E-commerce Platforms
+Consumer Goods Distribution
+Media and Entertainment
+
+Nollywood (Film Industry)
+Music Industry
+Sports and Events Management
+Healthcare
+
+Pharmaceutical Production
+Hospitals and Clinics
+Medical Equipment
+Hospitality and Tourism
+
+Hotels and Resorts
+Ecotourism
+Education
+
+Universities and Private Schools
+EdTech
+Professional Services
+
+Legal Services
+Accounting and Consulting
+Nonprofit and NGOs
+
+Development and Aid Organizations
+Emerging Industries
+Renewable Energy
+Agribusiness Technology
+Creative Arts and Design
+}
