6187fb3d39ee90090a22d86515eb469413f0dd1f
abolarin babatunde <abolarinbabatunde88@gmail.com>
2024-11-26 02:45:38 -0800

    SUMMARY

--- GovernmentPr/resources/views/components/admin/role-management.blade.php
+++ GovernmentPr/resources/views/components/admin/role-management.blade.php
@@ -71,73 +71,19 @@
             select.addEventListener('change', async () => {
                 let guard = select.value
                 console.log('change detected', guard);
-                let url = "{{ route('admin.guard-change') }}"
+                let url = "{{ route('admin.guard-change') }}";
                 let formData = new FormData()
                 formData.append('guard', guard)
                 let response = await fetch(url, {
                     method: 'POST',
                     headers: {
-                        'X-CSRF-TOKEN' : '{{ csrf_token() }}',
+                        'X-CSRF-TOKEN':'{{ csrf_token() }}',
                     },
                     credentials: 'same-origin',
                     body: formData
                 }).then(async (response) => {
-                    let data = await response.json()
-                    console.log(data);
-                    if (data.roles) {
-                            let thead = document.querySelector('.guard-table thead tr')
-                            // let tbody = document.querySelector('.guard-table tbody')
-                            thead.innerHTML = `<th> ACTIONS </th>`
-                            data.roles.forEach(role => {  thead.innerHTML+= `<th>${role.name}</th>`; })
-                            // tbody.innerHTML = ""
-                            let tBody = document.querySelector('.guard-table tbody');
-                            console.log(tBody);
-                            tBody.innerHTML = ``
-
-                            data.permissions.forEach(async (permission) => { 
-                                tBody.innerHTML += `
-                                        <tr data-${permission.id}>
-                                        <td>${permission.name}</td>
-                                        </tr>`
-                                let permissionTr = document.querySelector(`.guard-table tbody tr[data-${permission.id}]`)
-
-                                data.roles.forEach(role => {
-                                let url = "{{ route('admin.fetch.role-permission') }}"
-                                let formData = new FormData();
-                                formData.append('role_id', role.id)
-                                formData.append('permission_id', permission.id)
-                                    let response = fetch(url, {
-                                        method: "POST",
-                                        headers: {
-                                            'X-CSRF-TOKEN': '{{ csrf_token() }}',
-                                        },
-                                        credentials: 'same-origin',
-                                        body: formData
-                                    }).then(async response => {
-                                        let data = await response.json();
-                                        console.log(data);
-                                        
-                                        console.log(permissionTr);
-                                        permissionTr.innerHTML += `
-                                        <td>
-                                            <div class="form-check form-switch">
-                                                <input class="form-check-input" 
-                                                type="checkbox" 
-                                                role="switch" 
-                                                ${(data.role_permission != null)? "checked":""} 
-                                                onclick="updateRolePermission(this,${role.id}, ${permission.id})">
-                                            </div>
-                                        </td>
-                                        `
-                                        
-                                    })
-                                })
-
-                            })
-                    }
-                }).catch((err) => {
-                    console.log("--ERROR: ", err);
-                });
+                    console.log(await response.json());
+                })
             })
         }
     </script>
