72d2d565467099df3d213a4c2d2dd412a3d286e6
Ayomides <108183340+Ayomides@users.noreply.github.com>
2024-12-12 11:49:51 +0100

    update

--- GovernmentPr/routes/web.php
+++ GovernmentPr/routes/web.php
@@ -14,12 +14,6 @@ use App\Http\Controllers\HomePageController;
 |
 */
 
-<<<<<<< HEAD
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/home', [HomePageController::class, 'displayHome'])->name('home');
-=======
     // Route::get('/', function () {
     //     return Inertia::render('Welcome', [
     //         'canLogin' => Route::has('login'),
@@ -28,7 +22,6 @@ Route::get('/home', [HomePageController::class, 'displayHome'])->name('home');
     //         'phpVersion' => PHP_VERSION,
     //     ]);
     // });
->>>>>>> parent of 7b9c021 (Merge branch 'main' of https://github.com/Joseph-67/Governnment-Web)
 
 Route::middleware([
     'auth:sanctum',
