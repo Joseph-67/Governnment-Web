095b4d4b0e1d216ddc89c71a897917205bf47e99
abolarin babatunde <abolarinbabatunde88@gmail.com>
2025-01-07 16:18:47 +0100

    summary

--- GovernmentPr/routes/admin.php
+++ GovernmentPr/routes/admin.php
@@ -152,6 +152,7 @@ Route::prefix('admin')->middleware('auth:admin')->group(function() {
 
     Route::controller(CompanyMaterialController::class)->group(function(){
         Route::post('/company/material-setup', 'store')->name('admin.save-company-material');
+        Route::post('/company/material-setup/price', 'store_price')->name('admin.save-company-material-price');
     });
 
     Route::controller(CategoryController::class)->group(function(){
