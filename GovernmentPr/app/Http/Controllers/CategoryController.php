f551983ffc7c1bda53706e03c56f5da64c033df6
abolarin123 <carteratuma@gmail.com>
2025-01-08 11:50:17 +0100

    update

--- GovernmentPr/app/Http/Controllers/CategoryController.php
+++ GovernmentPr/app/Http/Controllers/CategoryController.php
@@ -25,7 +25,8 @@ class CategoryController extends Controller
     public function create()
     {
         //
-        return view("components.apps.category");
+        $data['categoryList']=category::get();
+        return view("components.apps.category", $data);
     }
 
     /**
--- GovernmentPr/resources/views/components/apps/category.blade.php
+++ GovernmentPr/resources/views/components/apps/category.blade.php
@@ -44,4 +44,47 @@
         </div>
     </div>
 </div>
+
+<div class="card">
+                            <div class="card-header">
+                                <div class="row align-items-center">
+                                    <div class="col">
+                                        <h4 class="card-title">Category</h4>
+                                    </div><!--end col-->
+                                </div> <!--end row-->
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered mb-0 table-centered" id="tbl-company-material">
+                                        <thead>
+                                        <tr>
+                                            <th>Category Name</th>
+                                            <th>Category Description</th>
+                                            <th class="text-end">Action</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($categoryList as $category_list)
+                                            <tr>
+                                                <td>{{ $category_list -> category_name }}</td>
+                                                <td>{{$category_list -> category_description  }}</td>
+                                                <td class="text-end">
+                                                    <div class="dropdown d-inline-block">
+                                                        <a class="dropdown-toggle arrow-none" id="dLabel11" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                                                            <i class="las la-ellipsis-v fs-20 text-muted"></i>
+                                                        </a>
+                                                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dLabel11">
+                                                            <a class="dropdown-item" href="#">Open Category</a>
+                                                            <a class="dropdown-item" href="#">Update Category</a>
+                                                            <a class="dropdown-item" href="#">Delete Category</a>
+                                                            <a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">Setup Price</a>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table><!--end /table-->
+                                </div><!--end /tableresponsive-->
+                            </div>
 </x-layouts.admin-app>
