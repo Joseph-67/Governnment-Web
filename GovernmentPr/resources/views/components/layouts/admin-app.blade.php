eb0d40a5841c20fd9a38c21e0ccb399e40733aba
abolarin babatunde <abolarinbabatunde88@gmail.com>
2025-03-25 23:07:53 +0100

    Update admin-app.blade.php

--- GovernmentPr/resources/views/components/layouts/admin-app.blade.php
+++ GovernmentPr/resources/views/components/layouts/admin-app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <title>@yield('PageTitle') – Federal Ministry of Environment</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta content="Federal Ministry of Environment" name="description" />
     <meta content="" name="author" />
