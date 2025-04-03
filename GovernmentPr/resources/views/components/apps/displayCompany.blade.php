8c53f3f30c686a6767e303b5d8d06ce873b59673
Joseph-67 <carteratuma@gmail.com>
2025-04-03 05:06:21 -0700

    Update displayCompany.blade.php

--- GovernmentPr/resources/views/components/apps/displayCompany.blade.php
+++ GovernmentPr/resources/views/components/apps/displayCompany.blade.php
@@ -65,7 +65,7 @@
                                             </a>
                                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dLabel11">
                                                 <a class="dropdown-item" href="{{ route('admin.show-company', ['company'=> encrypt($company->company_id)]) }}">Open Company</a>
-                                                <a class="dropdown-item" href="{{ route('admin.show-company', ['company'=> encrypt($company->company_id)]) }}">Assign Users to Company</a>
+                                                <a class="dropdown-item" href="#"  data-bs-toggle="modal" data-bs-target="#staticBackdrop">Assign Users to Company</a>
                                                 <form action="{{ route('admin.show-company', ['company'=> encrypt($company->company_id)]) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this company?');">
                                                     @csrf
                                                     @method('DELETE')
@@ -82,7 +82,27 @@
                 </div>
             </div>
         </div> <!-- end col -->
-    </div> <!-- end row -->                                     
+    </div> <!-- end row -->  
+            <!-- modalmodal to assign users -->
+        <!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg"> <!-- Added 'modal-lg' to increase modal width -->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            .....
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Understood</button>
+            </div>
+        </div>
+    </div>
+</div>
+            <!-- end modal -->
 </div><!-- container -->
 
 </x-layouts.admin-app>
