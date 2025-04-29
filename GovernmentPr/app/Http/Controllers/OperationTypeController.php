1c9d51ae788baf9d2d46bb8140de536c099babcd
Joseph-67 <carteratuma@gmail.com>
2025-04-29 17:25:34 +0100

    update

--- GovernmentPr/app/Http/Controllers/OperationTypeController.php
+++ GovernmentPr/app/Http/Controllers/OperationTypeController.php
@@ -152,20 +152,21 @@ class OperationTypeController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function destroy(OperationType $operationType)
-    {
-        //
-        // dd($operationType->operation_type_id);
-        $operationType = OperationType::find($operationType->operation_type_id);
-        if ($operationType) {
-            $operationType->forceDelete(); // Permanently delete the category
-
-            $operationType = OperationType::get();
-            return response()->json([
-                'status' => 'success',
-                'message' => 'Operation Type deleted successfully',
-                'operation_types' => $operationType]);
-        } else {
-            return response()->json(['status' => 'error', 'message' => 'Operation Type not found']);
-        }
+{
+    if ($operationType) {
+        $operationType->forceDelete(); // Force delete
+        $operationTypes = OperationType::all(); // Renamed for clarity
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Operation Type deleted successfully',
+            'operation_types' => $operationTypes
+        ]);
+    } else {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Operation Type not found'
+        ], 404);
     }
+}
+
 }
