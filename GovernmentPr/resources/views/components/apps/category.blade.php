0ccccda972d7e60cc2fcf27b1cf7e9e225f9fe92
ellastar-b <stellamoses081@gmail.com>
2025-05-16 12:52:38 +0100

    Update category.blade.php

--- GovernmentPr/resources/views/components/apps/category.blade.php
+++ GovernmentPr/resources/views/components/apps/category.blade.php
@@ -178,22 +178,6 @@
                                         <th class="text-end">Action</th>
                                     </tr>
                                 </thead>
-                                <tbody>
-                                    @foreach($WasteSubCategoryList as $subcategory)
-                                        <tr>
-                                            <td>{{ $subcategory->waste_sub_category_name??"" }}</td>
-                                            <td>{{ $subcategory->waste_sub_category_description??"" }}</td>
-                                            <td class="text-end">
-                                                <button class="btn btn-sm btn-warning me-2" type="button" onclick='editSubcategory("{{ $subcategory->id }}", "{{ $subcategory->waste_sub_category_name }}")'>
-                                                    <i class="las la-edit me-1"></i> Edit
-                                                </button>
-                                                <button class="btn btn-sm btn-danger" type="button" onclick='deleteSubcategory("{{ $subcategory->id }}", "{{ $subcategory->waste_sub_category_name }}")'>
-                                                    <i class="las la-trash-alt me-1"></i> Delete
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
                             </table>
                         </div>
                     </div>
