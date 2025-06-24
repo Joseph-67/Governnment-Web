389e1b51924676db1ff700d7ca8489a852d4e4fd
Abolarin123 <abolarinbabatunde88@gmail.com>
2025-06-24 08:34:12 +0100

    Revert "Update companyProfile.blade.php"
    This reverts commit 04086a043588b91e017245b586852c820d331996.

--- GovernmentPr/resources/views/components/apps/companyProfile.blade.php
+++ GovernmentPr/resources/views/components/apps/companyProfile.blade.php
@@ -12812,6 +12812,7 @@
 
     // Edit workflow step
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     window.editWorkflowStep = function(id) {
         // Fetch workflow step details and populate the form for editing
         // (Implementation depends on your backend API)
@@ -12839,6 +12840,7 @@
             modal.show();
         }
 =======
+=======
 >>>>>>> Stashed changes
     window.editWorkflowStep = function (id) {
         const row = $(`button[onclick="editWorkflowStep('${id}')"]`).closest('tr');
@@ -12862,6 +12864,10 @@
         }
         const modal = new bootstrap.Modal(document.getElementById('workflowEditModal'));
         modal.show();
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
     };
 
     // Delete workflow step
